24188c7d961f263b32e7fcb431d96b1aecb12c60
BiswajitMakhal <makhalbiswajit1@gmail.com>
2026-05-02 04:23:55 +0530

    nodemailer update

--- app/config/nodemailerConfig.js
+++ app/config/nodemailerConfig.js
@@ -1,13 +1,24 @@
 const nodemailer = require("nodemailer");
 
+// Port 465 hole secure true hobe, 587 hole false hobe
+const emailPort = Number(process.env.EMAIL_PORT) || 587;
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT),
-  secure: true,
+  host: process.env.EMAIL_HOST || "smtp.gmail.com",
+  port: emailPort,
+  secure: emailPort === 465, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.error("Nodemailer Connection Error:", error);
+  } else {
+    console.log("Nodemailer is Ready to send emails!");
+  }
 });
 
 module.exports = transporter;
