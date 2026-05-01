665ac627edcd9117128590bb1d24b121f4d16084
BiswajitMakhal <makhalbiswajit1@gmail.com>
2026-05-02 03:59:03 +0530

    nodemailer update for render

--- app/config/nodemailerConfig.js
+++ app/config/nodemailerConfig.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: Number(process.env.EMAIL_PORT),
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
