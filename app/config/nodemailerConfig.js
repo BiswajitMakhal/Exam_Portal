453611500a61b7dd47124ba3089798184279cd1a
BiswajitMakhal <makhalbiswajit1@gmail.com>
2026-05-02 05:39:56 +0530

    nodemailer update for render

--- app/config/nodemailerConfig.js
+++ app/config/nodemailerConfig.js
@@ -1,22 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: 465,
-  secure: true, 
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
+    pass: process.env.EMAIL_PASS,
   },
-  family: 4 // for render deploy (forcefully)
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.error(" NODEMAILER ERROR IN RENDER:", error);
-  } else {
-    console.log("Nodemailer is completely READY on Render!");
-  }
+  family: 4 //for render deploying
 });
 
 module.exports = transporter;
